73599d3e6354b148dc53f16d6927c8f69671160f
Glitch (glitch-hello-website) <none>
2022-08-16 11:17:23 +0000

    🚍🕌 Checkpoint ./index.js:44665776/567 ./.glitch-assets:44665776/381

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const canvas = document.querySelector("canvas")
-const c = canvas.getcontext('2D')
+const c = canvas.getContext('2d')
 
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+canvas.width = innerWidth
+canvas.height = innerHeight
 
 
 
 class Player {
   constructor(){
-    this.position= {
+    this.position = {
       x: 200,
       y: 200
     }
@@ -18,17 +18,28 @@ class Player {
       y: 0 
     }
     
-    //this.image = 
+    const image = new Image()
+    image.src = 'https://cdn.glitch.global/60d94363-1073-4daf-aa5b-1d90c575d322/NachoGif.png?v=1660648241228'//Transport this to ./imgfolder eventually
     
-    this.width = 100;
+    
+    this.image = image
+    this.width = 100
     this.height = 100
 
   }
   draw(){
-    c.fillStyle = "red"
-    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    //c.fillStyle = "red"
+    //c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    c.drawImage(this.image, this.position.x, this.position.y)
   }
 }
 
 const player = new Player()
 player.draw()
+
+function animate(){
+  requestAnimationFrame(animate)
+  player.draw()
+}
+
+animate()
