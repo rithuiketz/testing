f9da8df4f86b12f4be5dffcdbacc267a0d00bc8d
Glitch (glitch-hello-website) <none>
2022-08-16 11:06:22 +0000

    ⏰🚤 Checkpoint ./index.js:44665776/554 ./index.html:44665776/118

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 const canvas = document.querySelector("canvas")
-console.log(canvas);
+const c = canvas.getcontext('2D')
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+
+
+class Player {
+  constructor(){
+    this.position= {
+      x: 200,
+      y: 200
+    }
+    
+    this.velocity = {
+      x: 0,
+      y: 0 
+    }
+    
+    //this.image = 
+    
+    this.width = 100;
+    this.height = 100
+
+  }
+  draw(){
+    c.fillStyle = "red"
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+  }
+}
+
+const player = new Player()
+player.draw()
