daa6209040bb86a4784dd78ad2534ee015b7443b
Glitch (glitch-hello-website) <none>
2022-09-02 10:21:28 +0000

    👋🌈 Checkpoint ./index.js:44665776/1242 ./index.html:44665776/147 ./style.css:44665776/293

--- index.js
+++ index.js
@@ -243,30 +243,10 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phon
       keys.d.pressed = false;
     });
   };
+  
+  screen.orientation.lock("portrait");
+  
+  console.log("P")
 }
 
 
-//Device Turning message
-jQuery(window).bind('orientationchange', function(e) {
- switch ( window.orientation ) {
-  case 0:
-    $('.turnDeviceNotification').css('display', 'none');
-    // The device is in portrait mode now
-  break;
-
-  case 180:
-    $('.turnDeviceNotification').css('display', 'none');
-    // The device is in portrait mode now
-  break;
-
-  case 90:
-    // The device is in landscape now
-    $('.turnDeviceNotification').css('display', 'block');
-  break;
-
-  case -90:
-    // The device is in landscape now
-    $('.turnDeviceNotification').css('display', 'block');
-  break;
- }
-});
