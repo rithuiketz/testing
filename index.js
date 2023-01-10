be061282bd3bf295ef55fe75cf7d06512172342d
Glitch (glitch-hello-website) <none>
2023-01-10 13:39:00 +0000

    🐁🎾 Checkpoint ./index.js:44665776/355 ./index.html:44665776/980

--- index.js
+++ index.js
@@ -303,16 +303,7 @@ function animate(Player) {
   
 }
 
-function deleteAllCookies() {
-    const cookies = document.cookie.split(";");
 
-    for (let i = 0; i < cookies.length; i++) {
-        const cookie = cookies[i];
-        const eqPos = cookie.indexOf("=");
-        const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    }
-}
 
 //// Event Listeners for UI////
 
