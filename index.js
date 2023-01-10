4277a75387ebc24c7d30f2fb7feca9a0dd15dbd5
Glitch (glitch-hello-website) <none>
2023-01-10 13:26:56 +0000

    🏓🎍 Checkpoint ./index.js:44665776/1069

--- index.js
+++ index.js
@@ -303,6 +303,17 @@ function animate(Player) {
   
 }
 
+function deleteAllCookies() {
+    const cookies = document.cookie.split(";");
+
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i];
+        const eqPos = cookie.indexOf("=");
+        const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
+
 //// Event Listeners for UI////
 
 addEventListener("keydown", ({ key }) => {
@@ -431,3 +442,5 @@ if (
 
   screen.orientation.lock("portrait");
 }
+
+
